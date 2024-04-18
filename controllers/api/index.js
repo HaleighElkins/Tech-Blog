ba954ce52b7ad7e0909a9a5e99aefd2c46197740
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-04-18 10:07:05 -0600

    added in api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const userRoutes = require('./userRoutes');
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router
+.use('/users',userRoutes)
+.use('/blogs',blogRoutes)
+.use('/comments', commentRoutes);
+
+module.exports = router;
+
+
+
