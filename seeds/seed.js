5cb6f618bdee69e99e4995d309dc85c466bd6e33
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-04-18 10:33:25 -0600

    added in content in the seeds

---
+++ seeds/seed.js
@@ -0,0 +1,10 @@
+[
+    { "content": "Fascinating insights!" },
+    { "content": "That's an intriguing perspective." },
+    { "content": "Excellent post, incredibly insightful!" },
+    { "content": "I appreciate the fresh perspective, thank you for sharing." },
+    { "content": "Engaging read, eagerly awaiting more content like this." },
+    { "content": "I'm curious about a particular point, could you provide more detail?" },
+    { "content": "This blog has been immensely beneficial for my project." }
+  ]
+  
