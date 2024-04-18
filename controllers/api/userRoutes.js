9961bd4dee7d23cd1c837682aaa0fc6f890fe485
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-04-18 10:27:34 -0600

    added in api routes and finshed userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -9,3 +9,37 @@ router.use((req, res, next) => {
     next();
 });
 
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json(userData);
+    } catch (err) {
+      res.handleBadRequest(err);
+    }
+  });
+  
+  router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+      if (!userData || !(await userData.checkPassword(req.body.password))) {
+        res.handleUnauthorized();
+        return;
+      }
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({ user: userData, message: 'You are now logged in!' });
+    } catch (err) {
+      res.handleBadRequest(err);
+    }
+  });
+  
+  router.post('/logout', (req, res) => {
+    if (!req.session.logged_in) {
+      return res.status(404).end();
+    }
+    req.session.destroy(() => res.status(204).end());
+  });
+  
+  module.exports = router;
