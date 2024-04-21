79e3a6ed711b46565cf0c862285aaef62862079b
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-04-21 16:00:18 -0600

    started server.js added in connections

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
