a047e48fa46d010afed76254d926bd18e70cf7c0
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-04-21 16:26:23 -0600

    added in consts for session config, added in app use, app set and app engine

--- server.js
+++ server.js
@@ -10,3 +10,36 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const hbs = exphbs.create({ helpers });
+
+const sessionConfig = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 444444,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sessionConfig));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
