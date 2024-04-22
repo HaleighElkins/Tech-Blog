9c12648d2ae9eb795f643824b2da4bad6f69bfad
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-04-22 10:32:12 -0600

    working on bugs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const blogRoutes = require('./controllers/api/blogRoutes')
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -16,7 +17,7 @@ const hbs = exphbs.create({ helpers });
 const sessionConfig = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 444444,
+    maxAge: 999999,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
