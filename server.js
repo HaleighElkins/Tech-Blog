63108d57f42770a83c247739d503b603b4b46c4a
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-05-09 21:17:03 -0600

    working on bug, the css nor the handlebars are showing correctly

--- server.js
+++ server.js
@@ -1,3 +1,111 @@
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+// const blogRoutes = require('./controllers/api/blogRoutes')
+// const helpers = require('./utils/helpers');
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// const hbs = exphbs.create({ helpers });
+
+// const sessionConfig = {
+//   secret: 'Super secret secret',
+//   cookie: {
+//     maxAge: 999999,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict',
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sessionConfig));
+// app.use('/', blogRoutes);
+// // app.use('/api/blogs', blogRoutes);
+
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
+
+// sequelize.sync({ force: false })
+//   .then(() => {
+//     app.listen(PORT, () => console.log('Now listening'));
+//   })
+//   .catch(err => {
+//     console.error('Error starting server:', err);
+//   });
+
+
+
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+// const blogRoutes = require('./controllers/api/blogRoutes')
+// const helpers = require('./utils/helpers');
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// const hbs = exphbs.create({ helpers });
+
+// const sessionConfig = {
+//   secret: 'Super secret secret',
+//   cookie: {
+//     maxAge: 999999,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict',
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sessionConfig));
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/api/blogs', blogRoutes); // Mount blogRoutes at /api/blogs
+
+// app.use(routes);
+
+// sequelize.sync({ force: false })
+//   .then(() => {
+//     app.listen(PORT, () => console.log('Now listening'));
+//   })
+//   .catch(err => {
+//     console.error('Error starting server:', err);
+//   });
+
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -38,6 +146,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api/blogs', blogRoutes); 
+
 app.use(routes);
 
 sequelize.sync({ force: false })
@@ -47,4 +157,3 @@ sequelize.sync({ force: false })
   .catch(err => {
     console.error('Error starting server:', err);
   });
-
